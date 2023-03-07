cf6a3cdb8e0c0b99240733069c2ee716bf1da82f
David <david.journo.31@gmail.com>
2023-03-07 19:10:53 +0100

    Adding new credential helpers
    These are responsible of deleting the credentials after the import is done.

--- constants.js
+++ constants.js
@@ -10,6 +10,9 @@ export const TASK_TYPE = 'http://redpencil.data.gift/vocabularies/tasks/Task';
 export const ERROR_TYPE= 'http://open-services.net/ns/core#Error';
 export const ERROR_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
 
+export const BASIC_AUTH = 'https://www.w3.org/2019/wot/security#BasicSecurityScheme';
+export const OAUTH2 = 'https://www.w3.org/2019/wot/security#OAuth2SecurityScheme';
+
 export const PREFIXES = `
   PREFIX harvesting: <http://lblod.data.gift/vocabularies/harvesting/>
   PREFIX terms: <http://purl.org/dc/terms/>
@@ -17,9 +20,16 @@ export const PREFIXES = `
   PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
   PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
+  PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
   PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX oslc: <http://open-services.net/ns/core#>
   PREFIX cogs: <http://vocab.deri.ie/cogs#>
   PREFIX adms: <http://www.w3.org/ns/adms#>
+  PREFIX dgftSec: <http://lblod.data.gift/vocabularies/security/>
+  PREFIX dgftOauth: <http://kanselarij.vo.data.gift/vocabularies/oauth-2.0-session/>
+  PREFIX wotSec: <https://www.w3.org/2019/wot/security#>
+  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
+  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
 `;
